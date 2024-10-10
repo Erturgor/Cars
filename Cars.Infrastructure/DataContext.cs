70d687a7f1874dfdd504047876554bd34b47eb9e
coolm <coolman02nl@gmail.com>
2024-10-10 14:06:41 +0200

    Dodaj pliki projektów.

---
+++ Cars.Infrastructure/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Cars.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cars.Infrastructure
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options) { }
+
+        // Cars to nazwa tabeli w bazie danych
+        public DbSet<Car> Cars { get; set; }
+    }
+}
